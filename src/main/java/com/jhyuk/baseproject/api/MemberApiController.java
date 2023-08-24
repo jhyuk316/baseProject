dc31db2873ba1a0e140a607504b00943ea849312
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2023-08-25 01:33:19 +0900

    addMember endpoint 추가

--- src/main/java/com/jhyuk/baseproject/api/MemberApiController.java
+++ src/main/java/com/jhyuk/baseproject/api/MemberApiController.java
@@ -3,39 +3,45 @@ package com.jhyuk.baseproject.api;
 
 import com.jhyuk.baseproject.api.dto.AddMemberRequest;
 import com.jhyuk.baseproject.entity.Member;
+import com.jhyuk.baseproject.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("member")
 public class MemberApiController {
 
+    private final MemberService memberService;
+
     @GetMapping
-    private ResponseEntity<List<Member>> getMembers(){
+    private ResponseEntity<List<Member>> getMembers() {
 
         return ResponseEntity.ok().body(List.of(new Member()));
     }
 
     @GetMapping("{memberId}")
-    private  ResponseEntity<Member> getMember(@PathVariable long memberId){
+    private ResponseEntity<Member> getMember(@PathVariable long memberId) {
 
         return ResponseEntity.ok().body(new Member());
     }
-    @PostMapping
-    private ResponseEntity<Member> addMember(AddMemberRequest addMemberRequest){
 
-        return ResponseEntity.ok().body(new Member());
+    @PostMapping
+    private ResponseEntity<Member> addMember(AddMemberRequest addMemberRequest) {
+        Member member = memberService.addMember(addMemberRequest);
+        return ResponseEntity.ok().body(member);
     }
 
     @DeleteMapping("{memberId}")
-    private ResponseEntity<String> deleteMember(@PathVariable long memberId){
+    private ResponseEntity<String> deleteMember(@PathVariable long memberId) {
 
         return ResponseEntity.ok().body("");
     }
-
-
 
 
 }
--- src/main/java/com/jhyuk/baseproject/api/dto/AddMemberRequest.java
+++ src/main/java/com/jhyuk/baseproject/api/dto/AddMemberRequest.java
@@ -1,7 +1,13 @@
 package com.jhyuk.baseproject.api.dto;
 
-import lombok.Data;
+import com.jhyuk.baseproject.entity.Member;
 
 public record AddMemberRequest(String userId, String email, String password) {
 
+    public Member toEntity() {
+        Member member = new Member();
+        member.setEmail(this.email);
+        member.setPassword(this.password);
+        return member;
+    }
 }
