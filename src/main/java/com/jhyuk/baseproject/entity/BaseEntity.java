945fc0580e07efc7c8ecb95a9cdea133e7ed4add
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2023-08-26 02:03:20 +0900

    BaseEntity 추가

---
+++ src/main/java/com/jhyuk/baseproject/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.jhyuk.baseproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private ZonedDateTime createdDate;
+
+    @LastModifiedDate
+    private ZonedDateTime updateDate;
+}
--- src/main/java/com/jhyuk/baseproject/entity/Member.java
+++ src/main/java/com/jhyuk/baseproject/entity/Member.java
@@ -1,20 +1,16 @@
 package com.jhyuk.baseproject.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-@Entity
 @Getter
-@Setter
-public class Member {
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+public class Member extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int id;
 
     String userId;
 
